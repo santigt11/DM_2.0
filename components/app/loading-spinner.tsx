9158cb60e3603e1b33372a82ff9c868754649340
Eduard Prigoana <eduard@prigoana.com>
2025-10-09 23:06:29 +0300

    qobuz is dead

--- components/app/loading-spinner.tsx
+++
@@ -1,7 +0,0 @@
-export const LoadingSpinner = () => {
-  return (
-    <div className="flex items-center justify-center p-8">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-muted border-t-primary" />
-    </div>
-  )
-}
