c4feb35c458b4d4597c403a85391b63385ea7c8a
Julien Maille <julien.maille@gmail.com>
2026-01-02 23:16:21 +0100

    UI: move remove-from-playlist button before the menu button

--- js/ui.js
+++ js/ui.js
@@ -1028,7 +1028,8 @@ async showFullscreenCover(track, nextTrack, lyricsManager, audioPlayer) {
                     removeBtn.title = 'Remove from playlist';
                     removeBtn.innerHTML = '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/></svg>';
                     removeBtn.dataset.trackIndex = index;
-                    actionsDiv.appendChild(removeBtn);
+                    const menuBtn = actionsDiv.querySelector('.track-menu-btn');
+                    actionsDiv.insertBefore(removeBtn, menuBtn);
                 });
 
                 // Update header like button - hide for user playlists
