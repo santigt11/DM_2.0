5999f7312fd760b035c0fdbdf63f3b2de7f34c1a
binimum <uimax.bai@gmail.com>
2026-02-15 20:11:15 +0000

    Nope, turns out I did it wrong

--- js/lyrics.js
+++ js/lyrics.js
@@ -368,7 +368,7 @@ export class LyricsManager {
         return new Promise((resolve, reject) => {
             const script = document.createElement('script');
             script.type = 'module';
-            script.src = 'https://cdn.jsdelivr.net/npm/@uimaxbai/am-lyrics@latest/dist/src/am-lyrics.min.js';
+            script.src = 'https://cdn.jsdelivr.net/npm/@uimaxbai/am-lyrics/dist/src/am-lyrics.min.js';
 
             script.onload = () => {
                 if (typeof customElements !== 'undefined') {
